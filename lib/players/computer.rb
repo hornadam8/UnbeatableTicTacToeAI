4e3f1a0fd77a183128118b36519c6b9328173886
hornadam8 <hornadam8@gmail.com>
2020-05-06 12:05:10 -0600

    further refactoring of computer player algorithm

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,6 +7,15 @@ class Player::Computer < Player
   def move(board)
     @board = board
     @input = " "
+    check_win_combos
+    check_center
+    check_corners
+    check_edges
+    @move = @input + 1
+    @move.to_s
+  end
+
+  def check_win_combos
     WIN_COMBINATIONS.each do |combo|
       if @board.taken_and_equal(combo[0],combo[1]) && !@board.taken?((combo[2]+1).to_s)
         @input = combo[2]
@@ -16,15 +25,25 @@ class Player::Computer < Player
         @input = combo[1]
       end
     end
+  end
+
+  def check_center
     if (@input == " ") && !@board.taken?(@board.center)
       @input = 4
-    elsif (@input == " ") && @board.taken?(@board.center)
+    end
+  end
+
+  def check_corners
+    if (@input == " ")
       @board.corners.each do |corner|
         if !@board.taken?(corner)
           @input = corner.to_i - 1
         end
       end
     end
+  end
+
+  def check_edges
     if (@input == " ")
       @board.edges.each do |edge|
         if !@board.taken?(edge)
@@ -32,9 +51,6 @@ class Player::Computer < Player
         end
       end
     end
-    @move = @input + 1
-    @move.to_s
   end
-
 
 end
